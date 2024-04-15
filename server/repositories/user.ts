9ecf50dff6ffa295ee8c310644bbcd6ee01741fe
seta-phongvu <pvu@veritone.com>
2024-04-15 16:46:52 +0700

    add

--- server/repositories/user.ts
+++ server/repositories/user.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import Exception from '../exceptions/Exception';
 import { Role, User } from '../models/index';
