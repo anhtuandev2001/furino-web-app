f1a662b146cff07c63e84649efae8bf5f0e73d9e
seta-phongvu <pvu@veritone.com>
2024-04-24 16:37:30 +0700

    add

--- web/src/utils/constants/strapi.ts
+++ web/src/utils/constants/strapi.ts
@@ -1,4 +1,5 @@
-// const BASE_URL = 'https://furino-server.vercel.app/';
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'https://furino-server.vercel.app/';
+// const BASE_URL = 'http://localhost:3000/';
+const Province_URL = 'https://vapi.vnappmob.com/api/province'
 
-export { BASE_URL };
+export { BASE_URL, Province_URL };
